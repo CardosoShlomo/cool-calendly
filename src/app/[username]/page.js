e83081a55337b755f7d2e7055881ba3109756af7
CardosoShlomo <cardososhlomo@gmail.com>
2025-09-10 10:00:09 +0300

    importing container to [username] page.js

--- src/app/[username]/page.js
+++ src/app/[username]/page.js
@@ -1,9 +1,10 @@
 "use client";
+import Container from "@/components/Container";
 import { useParams } from "next/navigation";
 
 export default function BookingsPage() {
   const { username } = useParams();
-  
+
   return (
     <Container style={{ padding: 50 }}>
       <Container style={{ padding: 50 }}>
